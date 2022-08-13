1f3c4f79e10fc753e49b7a31f8b5f77c85d5af44
Dartegnian <root@dartegnian.com>
2022-08-13 17:32:40 +0800

    Call IDB on app load, set accent theme from IDB

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccentService } from '@services/accent-service.service';
+import { IdbService } from '@services/idb.service';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 	title = "Dartegnian's Portfolio";
+
+	constructor(
+		private idb: IdbService,
+		private accent: AccentService
+	) {}
+
+	async ngOnInit() {
+		this.idb.connectToIDB();
+		const accentIndex = (await this.idb.getData("Material You", "themeIndex"))["value"] || 0;
+		this.accent.setAccent(accentIndex)
+	}
 }

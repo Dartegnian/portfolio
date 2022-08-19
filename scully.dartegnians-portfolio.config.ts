d248b251a212fa96958c150a47522a98264de3fc
Dartegnian <root@dartegnian.com>
2022-08-19 19:56:56 +0800

    Rename files, update Scully import

--- scully.dartegnians-portfolio.config.ts
+++ scully.dartegnians-portfolio.config.ts
@@ -2,7 +2,7 @@ import { ScullyConfig } from '@scullyio/scully';
 
 /** this loads the default render plugin, remove when switching to something else. */
 import '@scullyio/scully-plugin-puppeteer';
-import './.scully/plugins/plugin';
+import './scully/plugins/plugin';
 
 export const config: ScullyConfig = {
 	projectRoot: './src',

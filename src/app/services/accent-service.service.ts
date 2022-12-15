292e4279ea7a012e472479c040c879a6f65323b1
Dartegnian <root@dartegnian.com>
2022-12-15 12:19:11 +0800

    Change Chrome's theme color when various colors are picked

--- src/app/services/accent-service.service.ts
+++ src/app/services/accent-service.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
 import { IdbService } from '@services/idb.service';
+import { Meta } from '@angular/platform-browser';
 
 @Injectable({
 	providedIn: 'root'
@@ -18,10 +19,26 @@ export class AccentService {
 		"tertiary",
 		"quaternary"
 	];
+	lightColors: Array<string> = [
+		"#CDE5FF",
+		"#9af6b3",
+		"#ffd7f0",
+		"#eedbff"
+	];
+	darkColors: Array<string> = [
+		"#004b74",
+		"#00522a",
+		"#722b65",
+		"#5a318a"
+	];
+	themeMode: "light" | "dark" = "light";
 	accentSubscription: Subject<number>;
 	activeIndex = 0;
 
-	constructor(private idb: IdbService) {
+	constructor(
+		private idb: IdbService,
+		private meta: Meta
+	) {
 		this.accentSubscription = new Subject();
 	}
 
@@ -29,6 +46,20 @@ export class AccentService {
 		this.activeIndex = index;
 		this.accentSubscription.next(index);
 		this.setThemeInIdb(index);
+		this.setMetaTagColor();
+	}
+	
+	public setThemeMode(mode: "light" | "dark") {
+		this.themeMode = mode;
+		this.setMetaTagColor();
+	}
+
+	public setMetaTagColor() {
+		if (this.themeMode === "light") {
+			this.meta.updateTag({name: "theme-color", content: this.lightColors[this.activeIndex]});
+		} else {
+			this.meta.updateTag({name: "theme-color", content: this.darkColors[this.activeIndex]});
+		}
 	}
 
 	setThemeInIdb(i: number) {

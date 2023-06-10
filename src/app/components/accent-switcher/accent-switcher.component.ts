bf956bd752cfdbb01309f2fc29531be87e33d703
Dartegnian <root@dartegnian.com>
2023-06-11 03:42:38 +0800

    Backport main dynamic M3 color code into portfolio

--- src/app/components/accent-switcher/accent-switcher.component.ts
+++ src/app/components/accent-switcher/accent-switcher.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { AccentService } from 'src/app/services/accent-service.service';
 
@@ -11,18 +11,31 @@ export class AccentSwitcherComponent implements OnInit, OnDestroy {
 	images: Array<string>;
 	selected: string;
 	accentSubscription: Subscription;
+	customImageSubscription: Subscription;
+	customImage: string | ArrayBuffer | null = null;
+	index: number = 1;
 
 	constructor(
-		private accent: AccentService
+		private accent: AccentService,
+		private changeDetectorRef: ChangeDetectorRef
 	) {
 		this.images = this.accent.images;
 		this.selected = this.images[this.accent.activeIndex];
+		this.customImage = this.accent.customImage;
+		this.index = this.accent.activeIndex;
+
+		this.customImageSubscription = this.accent.customImageSubscription.subscribe(
+			(image: string | ArrayBuffer) => {
+				this.customImage = image;
+			}
+		);
 
 		this.accentSubscription = this.accent.accentSubscription.subscribe(
 			(index: number) => {
-				setTimeout(() =>
-					this.selected = this.images[index]
-				, 25);
+				this.customImage = this.accent.customImage;
+				this.changeDetectorRef.detectChanges();
+				this.selected = this.images[index];
+				this.index = index;
 			}
 		);
 	}
@@ -32,6 +45,7 @@ export class AccentSwitcherComponent implements OnInit, OnDestroy {
 
 	ngOnDestroy(): void {
 		this.accentSubscription.unsubscribe();
+		this.customImageSubscription.unsubscribe();
 	}
 
 	changeAccent(image: number, imageName: string): void {
@@ -39,5 +53,39 @@ export class AccentSwitcherComponent implements OnInit, OnDestroy {
 			this.selected = this.images[image];
 			this.accent.setAccent(image);
 		}
+	}
+
+	async onFileSelected(event: any) {
+		const file = event.target.files[0];
+		this.customImage = await this.getFileDataUrl(file);
+		this.changeDetectorRef.detectChanges();
+		this.setCustomImage();
+	}
+
+	setCustomImage() {
+		if (this.index !== 0) {
+			this.accent.setCustomImage(this.customImage as string | ArrayBuffer);
+		}
+	}
+
+	removeCustomImage() {
+		this.customImage = null;
+		this.accent.setCustomImage(null, true);
+	}
+
+	private getFileDataUrl(file: File): Promise<string> {
+		return new Promise((resolve, reject) => {
+			const reader = new FileReader();
+
+			reader.onload = (event: any) => {
+				resolve(event.target.result);
+			};
+
+			reader.onerror = (event: any) => {
+				reject(event.target.error);
+			};
+
+			reader.readAsDataURL(file);
+		});
 	}
 }

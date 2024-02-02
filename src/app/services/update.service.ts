bc1fc147417dd38c209036d307ffe4ad829220f9
Dartegnian <root@dartegnian.com>
2024-02-02 21:04:31 +0800

    Sync update service with prod

--- src/app/services/update.service.ts
+++ src/app/services/update.service.ts
@@ -8,25 +8,31 @@ import { filter, interval, map } from "rxjs";
 export class UpdateService {
 
 	constructor(public updates: SwUpdate) {
-	  if (updates.isEnabled) {
-		interval(60000).subscribe(() => updates.checkForUpdate());
-	  }
+		if (updates.isEnabled) {
+			interval(60000).subscribe(() => updates.checkForUpdate());
+		}
 	}
-  
+
 	public checkForUpdates(): void {
 		this.updates.versionUpdates.
-		  pipe(filter((evt: { type: string; }): evt is VersionReadyEvent => evt.type === 'VERSION_READY'),
-			map(evt => ({
-			  type: 'UPDATE_AVAILABLE',
-			  current: evt.currentVersion,
-			  available: evt.latestVersion,
-			}))).subscribe(() => {
-			  this.promptUser();
+			pipe(
+				filter((evt: { type: string; }): evt is VersionReadyEvent => evt.type === 'VERSION_READY'),
+				map(evt => ({
+					type: 'UPDATE_AVAILABLE',
+					current: evt.currentVersion,
+					available: evt.latestVersion,
+				}))
+			).subscribe(() => {
+				this.promptUser();
 			});
-	  }
-  
+	}
+
 	private promptUser(): void {
-	  console.log('Updating PWA');
-	  this.updates.activateUpdate().then(() => document.location.reload()); 
+		console.log('Updating PWA');
+		this.updates.activateUpdate().then(() =>
+			setTimeout(() => {
+				document.location.reload()
+			}, 2000)
+		);
 	}
 }

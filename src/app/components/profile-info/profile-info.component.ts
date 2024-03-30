ef67eb298e21eaabbd33b3e44470be6439cdc5fe
Dartegnian <root@dartegnian.com>
2024-03-30 22:47:27 +0800

    Start work on redesigned info

--- src/app/components/profile-info/profile-info.component.ts
+++ src/app/components/profile-info/profile-info.component.ts
@@ -29,6 +29,32 @@ export class ProfileInfoComponent implements OnInit {
 			text: "linkedin.com/in/dartegnian"
 		},
 	];
+	subsectionList = [
+		{
+			header: "6",
+			subheader: "Years",
+			title: "Work Experience",
+			link: "https://linkedin.com/in/dartegnian"
+		},
+		{
+			header: "6",
+			subheader: "Years",
+			title: "Work Experience",
+			link: "https://linkedin.com/in/dartegnian"
+		},
+		{
+			header: "32",
+			subheader: "Repos",
+			title: "Public Repositories",
+			link: "https://linkedin.com/in/dartegnian"
+		},
+		{
+			header: "6",
+			subheader: "Years",
+			title: "Work Experience",
+			link: "https://linkedin.com/in/dartegnian"
+		}
+	];
 
 	constructor() {
 		this.description = "Hi, I'm Dartegnian Velarde! I’m an agile and determined DevOps engineer with a passion for curating innovative and responsive web applications, with highly-functional knowledge on various aspects of computer programming, web development, and the DevOps process. Communicating well with others, troubleshooting, and fluency in English are some of my other skills. Adaptability and conscientiousness are my predominant traits.";

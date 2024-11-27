e4fa070594f2884239c6a3fa61492b53a74be3d5
Tomás Palma <tomas@tomasps.com>
2024-11-27 21:45:25 +0100

    Misc Update
    Signed-off-by: Tomás Palma <tomas@tomasps.com>

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'portfolio.dartegnian.com'`, () => {
+  it(`should have as title 'tomasps.com'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('portfolio.dartegnian.com');
+    expect(app.title).toEqual('tomasps.com');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('portfolio.dartegnian.com app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('tomasps.com app is running!');
   });
 });

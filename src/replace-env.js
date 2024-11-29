c0279d1e61dc33fe853d7d79809aaf01be621050
Tomas <tomas@tomasps.com>
2024-11-29 21:06:10 +0100

    Fix for enviroments
    Signed-off-by: Tomas <tomas@tomasps.com>

--- src/replace-env.js
+++ src/replace-env.js
@@ -7,7 +7,7 @@ const setEnv = () => {
   const colors = require('colors');
   const appVersion = require('../package.json').version;
   require('dotenv').config({
-    path: 'src/.env'
+    path: '/.env'
   });
 // `environment.ts` file structure
   const envConfigFile = `export const environment = {

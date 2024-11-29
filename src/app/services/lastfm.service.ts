97dfce3b0dcb5d36e5b4fee88423c58a90c95d7c
Tomás Palma <tomas@tomasps.com>
2024-11-29 09:06:34 +0100

    Fix for the enviroment
    Signed-off-by: Tomás Palma <tomas@tomasps.com>

--- src/app/services/lastfm.service.ts
+++ src/app/services/lastfm.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { environment } from 'src/environments/environment';
+import { environment } from 'src/environments/environment.prod';
 
 
 @Injectable({

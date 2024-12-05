2809849d34f6acd00bb9d559800c44b8bf6acbef
Tomas <tomas@tomasps.com>
2024-12-05 20:03:56 +0100

    Added translation to missing components
    Signed-off-by: Tomas <tomas@tomasps.com>

--- src/app/components/life-at-a-glance/life-at-a-glance.component.ts
+++ src/app/components/life-at-a-glance/life-at-a-glance.component.ts
@@ -1,104 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
-	selector: 'life-at-a-glance',
-	templateUrl: './life-at-a-glance.component.html',
-	styleUrls: ['./life-at-a-glance.component.scss']
+  selector: 'life-at-a-glance',
+  templateUrl: './life-at-a-glance.component.html',
+  styleUrls: ['./life-at-a-glance.component.scss']
 })
 export class LifeAtAGlanceComponent implements OnInit {
-	featureIndex = 0;
-	features = [
-		{
-			title: "At a glance",
-			icon: "lightbulb",
-			link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-			shortDescription: "How did this get here?",
-			description: ["Click any of the cards below to know the activities I'm involved with my life at the moment. Cards will be updated as I pick up new interests and hobbies."],
-		},
-		{
-			title: "Development",
-			icon: "terminal",
-			link: "https://github.com/tresillo2017",
-			shortDescription: "Development's always been my thing. Always has and always will be.",
-			description: ["I've always loved working on personal projects or ideas of mine that involve writing code. Having an opportunity to implement ideas, whether the client's or otherwise, is always exciting.", "These days, I'm working on writing shell scripts for my Linux installation. I use Arch Linux btw."]
-		},
-		{
-			title: "Relationships",
-			icon: "sentiment_satisfied",
-			link: "https://www.instagram.com/toomas_ps/",
-			shortDescription: "Keeping up with friends is hard, but they're my main source of joy in life.",
-			description: ["I've spent a great time of my life thinking about what my main source of joy is. Turns out, it was simple: my friends!", "I'm currently involved in many friend groups and, while it's hard to keep up with all of them on a monthly basis, I still try my best. Sometimes, I keep up with friends through Instagram."]
-		},
-		{
-			title: "Music",
-			icon: "queue_music",
-			link: "https://tidal.com/browse/user/195041547",
-			shortDescription: "Why do I listen to weird genres these days? I'm not sure, but I like 'em.",
-			description: ["My day isn't really complete without playing a song on loop for hours on end, to be honest. I mostly listen to stuff on tidal and off my local FLAC collection.", "My main genres of music these days are pop, rock and indie"]
-		},
-		// {
-		// 	title: "Writing",
-		// 	icon: "edit",
-		// 	link: "https://dartegnian.com",
-		// 	shortDescription: "Blogging and writing about my life story has been my hobby since high school.",
-		// 	description: ["I write blog posts in my free time. It's been a hobby/tradition to let out and express my thoughts through writing. It's sad to see that I'm the only person in all of my friend groups who has a public blog.", "Even so, I keep on writing for myself."]
-		// },
-		// {
-		// 	title: "Learning",
-		// 	icon: "psychology",
-		// 	link: "https://www.youtube.com/results?search_query=web+development+tutorial",
-		// 	shortDescription: "Why would I let my skillset rot as technologies change? In tech, you need to keep up.",
-		// 	description: ["In the field of IT, you always need to keep your skills sharpened and stay updated to the latest technologies offered.", "That's why I use Skillsha—ah, no, I really just watch through YouTube."]
-		// },
-		// {
-		// 	title: "Growth",
-		// 	icon: "potted_plant",
-		// 	link: "https://en.wikipedia.org/wiki/Stoicism",
-		// 	shortDescription: "I dread stagnation, I always yearn for growth. I always want to keep growing.",
-		// 	description: ["I really dread stagnation. I feel like my life will come to a standstill if I don't do a major life change every year. Because of a personal story, I always have to keep moving and improving myself.", "One of my main skills is the ability to grow up in a short amount of time, as someone can attest."]
-		// },
-	];
-	feature: string;
+  featureIndex = 0;
+  features: any[] = [];
+  feature: string = '';
 
-	constructor() {
-		this.feature = this.features[this.featureIndex].title;
-	}
+  constructor(private translate: TranslateService) {}
 
-	ngOnInit(): void {
-	}
+  ngOnInit(): void {
+    this.translate.onLangChange.subscribe(() => {
+      this.translateFeatures();
+    });
+    this.translateFeatures();
+  }
 
-	setFeature(index: number) {
-		this.featureIndex = index;
-		this.feature = this.features[index].title;
-	}
+  translateFeatures(): void {
+    this.translate.get('FEATURES').subscribe((res: any[]) => {
+      this.features = res;
+      this.feature = this.features[this.featureIndex].title;
+    });
+  }
 
-	skipFeature(action: "previous" | "next") {
-		let newFeatureIndex = this.featureIndex;
-		if (action === "previous") {
-			if (this.featureIndex !== 1 && this.featureIndex !== 0) {
-				newFeatureIndex -= 1;
-			}
-		} else {
-			if (this.featureIndex !== this.features.length - 1) {
-				newFeatureIndex += 1;
-			}
-		}
-		this.setFeature(newFeatureIndex);
-	}
+  setFeature(index: number): void {
+    this.featureIndex = index;
+    this.feature = this.features[index].title;
+  }
 
-	shuffleFeature() {
-		let randomFeatureIndex: number;
+  skipFeature(action: 'previous' | 'next'): void {
+    let newFeatureIndex = this.featureIndex;
+    if (action === 'previous' && this.featureIndex > 0) {
+      newFeatureIndex -= 1;
+    } else if (action === 'next' && this.featureIndex < this.features.length - 1) {
+      newFeatureIndex += 1;
+    }
+    this.setFeature(newFeatureIndex);
+  }
 
-		do {
-			randomFeatureIndex = this.getRandomNumberFromFeatures();
-		} while (randomFeatureIndex === this.featureIndex);
-
-		this.setFeature(randomFeatureIndex);
-	}
-
-	getRandomNumberFromFeatures() {
-		const min = 1;
-		const max = (this.features.length - 1);
-		return Math.floor(Math.random() * (max - min + 1) + min);
-	}
+  shuffleFeature(): void {
+    let randomFeatureIndex: number;
+    do {
+      randomFeatureIndex = Math.floor(Math.random() * this.features.length);
+    } while (randomFeatureIndex === this.featureIndex);
+    this.setFeature(randomFeatureIndex);
+  }
 }
--- src/app/components/skill-info/skill-info.component.ts
+++ src/app/components/skill-info/skill-info.component.ts
@@ -1,71 +1,78 @@
 import { isPlatformBrowser } from '@angular/common';
 import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
-	selector: 'skill-info',
-	templateUrl: './skill-info.component.html',
-	styleUrls: ['./skill-info.component.scss']
+  selector: 'skill-info',
+  templateUrl: './skill-info.component.html',
+  styleUrls: ['./skill-info.component.scss']
 })
 export class SkillInfoComponent implements OnInit {
-	webAttributes = [
-		"focused",
-		"inspired",
-		"driven",
-		"oriented",
-		"impassioned"
-	];
-	webAttributeIndex = 0;
-	webAttributeCharacter = 0;
-	webAttribute = this.webAttributes[this.webAttributeIndex];
-	cursorInterval: ReturnType<typeof setTimeout> | undefined;
-	isBrowser: boolean = false;
+  webAttributes: string[] = [];
+  webAttributeIndex = 0;
+  webAttributeCharacter = 0;
+  webAttribute: string = '';
+  cursorInterval: ReturnType<typeof setTimeout> | undefined;
+  isBrowser: boolean = false;
 
-	constructor(
-		@Inject(PLATFORM_ID) private platformId: Object
-	) {
-		this.isBrowser = isPlatformBrowser(this.platformId);
-	}
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private translate: TranslateService
+  ) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+  }
 
-	ngOnInit(): void {
-		if (this.isBrowser) { 
-			this.cursorInterval = setInterval(() => this.writeType(), 100);
-		}
-	}
+  ngOnInit(): void {
+    this.translate.get('WEB_ATTRIBUTES').subscribe((res: string[]) => {
+      this.webAttributes = res;
+      this.webAttribute = this.webAttributes[this.webAttributeIndex];
+      if (this.isBrowser) {
+        this.cursorInterval = setInterval(() => this.writeType(), 100);
+      }
+    });
 
-	writeType() {
-		const activeText = this.webAttributes[this.webAttributeIndex].substring(0, this.webAttributeCharacter + 1);
-		this.webAttribute = activeText;
-		this.webAttributeCharacter++;
+    this.translate.onLangChange.subscribe(() => {
+      this.translate.get('WEB_ATTRIBUTES').subscribe((res: string[]) => {
+        this.webAttributes = res;
+        this.webAttributeIndex = 0;
+        this.webAttributeCharacter = 0;
+        this.webAttribute = this.webAttributes[this.webAttributeIndex];
+      });
+    });
+  }
 
-		if (activeText === this.webAttributes[this.webAttributeIndex]) {
+  writeType() {
+    const activeText = this.webAttributes[this.webAttributeIndex].substring(0, this.webAttributeCharacter + 1);
+    this.webAttribute = activeText;
+    this.webAttributeCharacter++;
 
-			clearInterval(this.cursorInterval);
-			setTimeout(() => {
-				this.cursorInterval = setInterval(() => this.deleteType(), 50);
-			}, 1500);
-		}
-	}
+    if (activeText === this.webAttributes[this.webAttributeIndex]) {
+      clearInterval(this.cursorInterval);
+      setTimeout(() => {
+        this.cursorInterval = setInterval(() => this.deleteType(), 50);
+      }, 1500);
+    }
+  }
 
-	deleteType() {
-		const activeText = this.webAttributes[this.webAttributeIndex].substring(0, this.webAttributeCharacter - 1);
-		this.webAttribute = activeText;
-		this.webAttributeCharacter--;
+  deleteType() {
+    const activeText = this.webAttributes[this.webAttributeIndex].substring(0, this.webAttributeCharacter - 1);
+    this.webAttribute = activeText;
+    this.webAttributeCharacter--;
 
-		if (activeText === "") {
-			clearInterval(this.cursorInterval);
+    if (activeText === "") {
+      clearInterval(this.cursorInterval);
 
-			if (this.webAttributeIndex === (this.webAttributes.length - 1)) {
-				this.webAttributeIndex = 0;
-			}
-			else {
-				this.webAttributeIndex++;
-			}
+      if (this.webAttributeIndex === (this.webAttributes.length - 1)) {
+        this.webAttributeIndex = 0;
+      } else {
+        this.webAttributeIndex++;
+      }
 
-			this.webAttributeCharacter = 0;
+      this.webAttributeCharacter = 0;
 
-			setTimeout(() => {
-				this.cursorInterval = setInterval(() => this.writeType(), 100);
-			}, 500);
-		}
-	}
+      setTimeout(() => {
+        this.cursorInterval = setInterval(() => this.writeType(), 100);
+      }, 500);
+    }
+  }
 }

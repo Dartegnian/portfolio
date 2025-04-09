32651cac117ff678e0d6e9a4b715b5c034fa17d4
Dartegnian <root@dartegnian.com>
2025-04-09 23:48:40 +0800

    Import fixes from main site

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,13 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-
+import { isPlatformBrowser, ViewportScroller } from '@angular/common';
+import { Component, inject, PLATFORM_ID } from '@angular/core';
+import { AccentSwitcherComponent } from "../accent-switcher/accent-switcher.component";
 
 @Component({
-    selector: 'app-footer',
-    templateUrl: './footer.component.html',
-    styleUrls: ['./footer.component.scss'],
-    imports: []
+	selector: 'app-footer',
+	templateUrl: './footer.component.html',
+	styleUrls: ['./footer.component.scss'],
+	imports: [AccentSwitcherComponent]
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
+	private platformId = inject<Object>(PLATFORM_ID);
+	private viewportScroller = inject(ViewportScroller);
+	isBrowser: boolean = isPlatformBrowser(this.platformId);
+
 	footerLinks = [
 		{
 			icon: "photo_camera",
@@ -15,25 +20,68 @@ export class FooterComponent implements OnInit {
 			text: "Instagram"
 		},
 		{
-			icon: "public",
-			link: "https://dartegnian.com/",
-			text: "https://dartegnian.com"
+			icon: "work",
+			link: "https://www.linkedin.com/in/dartegnian/",
+			text: "LinkedIn"
 		},
 		{
 			icon: "code",
 			link: "https://github.com/dartegnian",
-			text: "github.com/dartegnian"
+			text: "GitHub"
 		},
 		{
-			icon: "work",
-			link: "https://www.linkedin.com/in/dartegnian/",
-			text: "linkedin.com/in/dartegnian"
+			icon: "queue_music",
+			link: "https://www.last.fm/user/Dartegnian",
+			text: "Last.fm"
+		},
+		{
+			icon: "music_note",
+			link: "https://open.spotify.com/user/dartegnian",
+			text: "Spotify"
+		},
+		{
+			icon: "alternate_email",
+			link: "https://mastodon.social/@dartegnian",
+			text: "Mastodon"
+		},
+		{
+			icon: "palette",
+			link: "https://www.behance.net/Dartegnian",
+			text: "Behance"
+		},
+		{
+			icon: "school",
+			link: "https://scholar.google.com/citations?user=SPHbv8EAAAAJ",
+			text: "Google Scholar"
 		},
 	];
 
-  constructor() { }
+	siteLinks = [
+		{
+			icon: "edit_note",
+			link: "https://dartegnian.com",
+			text: "Home"
+		},
+		{
+			icon: "edit_note",
+			link: "https://blog.dartegnian.com",
+			text: "Blog"
+		},
+		{
+			icon: "work_history",
+			link: "https://portfolio.dartegnian.com",
+			text: "Portfolio"
+		},
+		{
+			icon: "web_stories",
+			link: "https://stories.dartegnian.com",
+			text: "Web Stories"
+		},
+	];
 
-  ngOnInit(): void {
-  }
+	constructor() { }
 
+	scrollToTop(): void {
+		this.viewportScroller.scrollToPosition([0, 0]);
+	}
 }
